b2e38c7578ff0bbd1d20b0ba2f2184ed94594c4d
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2018-03-13 16:49:16 +0200

    refactoring: небольшая оптимизация.

--- backend/apps/theater/views.py
+++ backend/apps/theater/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import DetailView
 from django.http import Http404
+from django.db.models import Count
 from .models import Play
+from apps.mit_calendar.models import Event
 
 
 class PlayDetailView(DetailView):
@@ -13,3 +15,16 @@ class PlayDetailView(DetailView):
         if not play:
             raise Http404('Отсутствуют спектали в БД.')
         return play
+
+    def get_context_data(self, **kwargs):
+        context = super(PlayDetailView, self).get_context_data(**kwargs)
+
+        context['future_events'] = Event.objects.future().values(
+            'pk', 'date', 'time', 'place__title', 'place__address', 'visitors_count'
+        ).annotate(Count('spectator')).filter(play=self.object)
+        for event in context['future_events']:
+            event['spectator__percent'] = int(event['spectator__count'] / event['visitors_count'] * 100)
+
+        context['roles'] = self.object.personplayrole_set.values('person__image', 'person__name', 'role', 'text').all()
+
+        return context
